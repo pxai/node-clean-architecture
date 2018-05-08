b9ffa8bf9433a3da84428472c67b778187c792ac
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-05-08 13:08:09 +0200

    Modified index, it works

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const { TodoEntityDAO } = require('./repository/TodoEntityDAO');
 const { GetTodos } = require('./architecture/usecases/getTodos');
 const { GetTodo } = require('./architecture/usecases/getTodo');
-const { Todos } = require('./architecture/domain/todos');
+const { InsertTodo } = require('./architecture/usecases/insertTodo');
+const { Todo } = require('./architecture/domain/todo');
 
 
 
 const getTodosBoundary = new GetTodos(new TodoEntityDAO());
 const getTodoBoundary = new GetTodo(new TodoEntityDAO());
+const insertTodoBoundary = new InsertTodo(new TodoEntityDAO());
 console.log(getTodosBoundary.get())
 console.log(getTodosBoundary.get(1))
+insertTodoBoundary.insert(new Todo('Build a clean arch') );
+insertTodoBoundary.insert(new Todo('Take over the World') );
+
+console.log(getTodosBoundary.get())
